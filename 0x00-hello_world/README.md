a471a6bd1bce623faff8b4ce29d46c451b4a0ff4
sallyelsayed203 <sallyelsayed2030@gmail.com>
2023-09-15 00:50:26 +0300

    0-isupper.c

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+# x00-hello_world
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+   * main - Entry point
+   *
+   * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
