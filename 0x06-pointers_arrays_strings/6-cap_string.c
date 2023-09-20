c51cb73caf48c8ea9ca05c6f5038c455e451308f
sallyelsayed203 <sallyelsayed2030@gmail.com>
2023-09-20 20:14:41 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,28 +7,24 @@
 */
 char *cap_string(char *str)
 {
-	int index = 0;
+	int count;
 
-	while (str[index])
+	count = 0;
+
+	while (str[count] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				index == 0)
-					str[index] -= 32;
-		index++;
+		if (str[0] >= 97 && str[0] <= 122)
+			str[0] = str[0] - 32;
+		if (str[count] == ' ' || str[count] == '\t' || str[count] == '\n'
+				 || str[count] == ',' || str[count] == ';' || str[count] == '.'
+				 || str[count] == '.' || str[count] == '!' || str[count] == '?'
+				 || str[count] == '"' || str[count] == '(' || str[count] == ')'
+				 || str[count] == '{' || str[count] == '}')
+		{
+			if (str[count + 1] >= 97 && str[count + 1] <= 122)
+				str[count + 1] = str[count + 1] - 32;
+		}
+		count++;
 	}
 	return (str);
 }
