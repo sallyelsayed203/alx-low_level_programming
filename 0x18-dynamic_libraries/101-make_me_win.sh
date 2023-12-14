83f1b3f2d4511053c688163f45c32ffa8c5afbc0
sallyelsayed203 <sallyelsayed2030@gmail.com>
2023-12-14 23:36:34 +0200

    finished

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -P ..https://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@github.com/sallyelsayed203/alx-low_level_programminging/master/0x18-dynamic_libraries/lib
+export LD_PRELOAD="$PWD/../libgiga.so"
